2640a272c80c5b7214f5255db4d552185e777215
Viren Sajju Dhanwani Dhanwani <viren.dhanwani.33>
2021-04-03 15:04:55 +0000

    function returns and for in added

--- src/for-loops-analysis.js
+++ src/for-loops-analysis.js
@@ -14,17 +14,19 @@
  * @see https://thecodebarbarian.com/for-vs-for-each-vs-for-in-vs-for-of-in-javascript
  */
 
-'use strict'
+'use strict';
 const { performance } = require('perf_hooks'); // performance.now()
-const ARRAY_LENGTH = 100000000;
+const ARRAY_LENGTH = 40000000;
 let array = Array.from({ length: ARRAY_LENGTH }, () => Math.floor(Math.random() * 1000000));
 
 /**
- * Computes the fourth power of a number 
+ * Computes the fourth power of a number
  * @param {Number} element
+ * @return {Number} result of the fourth power
  */
 const powFour = (element) => {
-  Math.pow(element, 4);
+  let result = Math.pow(element, 4);
+  return result;
 }
 
 /**
@@ -70,3 +72,16 @@ for (const element of array) {
 t1 = performance.now();
 time = t1 - t0;
 console.log(time + ' milisegundos');
+
+/**
+ * Analysis of the 'for ... in ...' for iterating (intended for objects)
+ */
+ t0 = performance.now();
+ console.log('For in:');
+ for (const element in array) {
+   powFour(element);
+ }
+ t1 = performance.now();
+ time = t1 - t0;
+ console.log(time + ' milisegundos');
+
